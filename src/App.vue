6a0886c74a8a5fa39394949d04793f11cb2f2d80
ritatanght <doggymint@gmail.com>
2024-01-26 17:26:40 -0500

    Include InputField component and pass down value

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import InputField from "./components/InputField.vue";
+export default {
+  data() {
+    return {
+      inputs: [
+        { label: "DAY", placeholder: "DD", value: "" },
+        { label: "MONTH", placeholder: "MM", value: "" },
+        { label: "YEAR", placeholder: "YYYY", value: "" },
+      ],
+    };
+  },
+  components: {
+    InputField,
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <form>
+      <div class="columns">
+        <InputField
+          v-for="input in inputs"
+          :label="input.label"
+          :placeholder="input.placeholder"
+        ></InputField>
+      </div>
+      <button class="submit-btn" aria-label="submit">
+        <img src="/assets/images/icon-arrow.svg" alt="" class="down-arrow" />
+      </button>
+    </form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<style>
+@font-face {
+  font-family: poppins-400i;
+  src: url("/assets/fonts/Poppins-Italic.ttf");
+}
+
+@font-face {
+  font-family: poppins-700;
+  src: url("/assets/fonts/Poppins-Bold.ttf");
+}
+
+@font-face {
+  font-family: poppins-800i;
+  src: url("/assets/fonts/Poppins-ExtraBoldItalic.ttf");
+}
+
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  background-color: hsl(0, 0%, 94%);
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+</style>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  background-color: white;
+  border-radius: 10px 10px 80px;
+  padding: 2em;
+  max-width: 800px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+form {
+  font-family: poppins-700;
+  border-bottom: 2px solid hsl(0, 0%, 86%);
+  position: relative;
+  padding-block: 3em;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.columns {
+  display: flex;
+  gap: 5%;
+  width: 70%;
+}
+
+.submit-btn {
+  background-color: hsl(259, 100%, 65%);
+  border: 0;
+  padding: 1.2em;
+  border-radius: 50%;
+  position: absolute;
+  bottom: -25%;
+  right: 0;
+  cursor: pointer;
+}
+
+.down-arrow {
+  max-width: 30px;
 }
 </style>
