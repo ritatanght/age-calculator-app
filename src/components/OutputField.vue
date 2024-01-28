b09bc1cbae26b6b3fe871bad3207d08fdcce59bb
ritatanght <doggymint@gmail.com>
2024-01-27 20:13:52 -0500

    Add vue3-autocounter to display the animated value

--- src/components/OutputField.vue
+++ src/components/OutputField.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <span class="value">{{ value }}</span
-    ><span>{{ unit }}</span>
+    <span class="value" v-if="value === '--'">{{ value }}</span
+    ><vue3-autocounter
+      class="value"
+      v-else
+      :startAmount="0"
+      :endAmount="value"
+      :duration="2"
+    />
+    <span>{{ unit }}</span>
   </div>
 </template>
 
 <script>
+import Vue3autocounter from "vue3-autocounter";
 export default {
   name: "OutputField",
   props: ["value", "unit"],
+  components: {
+    "vue3-autocounter": Vue3autocounter,
+  },
 };
 </script>
 
